1e2ce8ad5f5834d41dc828161c17e5618c5e4392
shubh <shahshubh1010@gmail.com>
2020-09-20 11:00:49 +0530

    changes

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/invoice-manager'));
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/invoice-manager/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
